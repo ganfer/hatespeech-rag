2fdd1eb485c99b31f3ab0e72d53f6da55eedc89f
Ferdinand Ganter <f.ganter@live.de>
2024-08-23 00:26:39 +0200

    Update Comment

--- initial_load/initial_load_ai.py
+++ initial_load/initial_load_ai.py
@@ -9,23 +9,24 @@ collection = chroma_client.get_or_create_collection(name="hatespeech")
 # Initialisiere das LLM Modell
 model = OllamaLLM(model="mannix/llama3.1-8b-abliterated")
 
-# Funktion, um Hassrede-Kategorien von llama3 zu generieren
-def generate_hate_speech_categories(model, prompt="Gebe weitere 100 Sätze auf deutsch aus, die als gruppenbezogene Menschenfeindlichkeit (Hassrede) identifiziert werden können. Wichtig ist, dass in jeder Aussage eine Zielgruppe existiert. Nutze keine Überschriften oder Aufzählungszeichen wie Zahlen oder Strichpunkte, beginne aber nach jeder Aussage eine neue Zeile."):
+# Funktion, um Hassrede von llama generieren zu lassen
+def generate_hate_speech(model, prompt="Gebe weitere 10 Sätze auf deutsch aus, die als gruppenbezogene Menschenfeindlichkeit (Hassrede) identifiziert werden können. Wichtig ist, dass in jeder Aussage eine Zielgruppe existiert. Nutze keine Überschriften oder Aufzählungszeichen wie Zahlen oder Strichpunkte, beginne aber nach jeder Aussage eine neue Zeile."):
     response = model.invoke(prompt)
     return response.splitlines()
 
-# Generiere Kategorien von Hassreden
-categories_list = generate_hate_speech_categories(model)
+# Generiere Hassrede
+hatespeech_list = generate_hate_speech(model)
 
-# Füge die Kategorien in die Datenbank ein
-for category in categories_list:
-    if category:  # Nur nicht-leere Kategorien hinzufügen
+# Füge die Hassrede in die Datenbank ein
+for hatespeech in hatespeech_list:
+    if hatespeech:  # Nur nicht-leere Sätze hinzufügen
         unique_id = str(uuid.uuid4())  # Generiere eine eindeutige ID
-        collection.upsert(documents=[category], ids=[unique_id])
+        collection.upsert(documents=[hatespeech], ids=[unique_id])
+#        print(hatespeech)
 
 # Alle Dokumente in der Sammlung abrufen
-documents = collection.get()
+#documents = collection.get()
 
-print("Gespeicherte Dokumente und ihre IDs:")
-for doc, id_ in zip(documents['documents'], documents['ids']):
-    print(f"ID: {id_}, Dokument: {doc}")
+#print("Gespeicherte Dokumente und ihre IDs:")
+#for doc, id_ in zip(documents['documents'], documents['ids']):
+#    print(f"ID: {id_}, Dokument: {doc}")
